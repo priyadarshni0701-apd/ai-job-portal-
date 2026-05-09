9d442e0ebe82a3d3ae879a1c63a9ef48ecc646e5
priyadarshni0701-apd <priyadarshni0701@gmail.com>
2026-05-09 07:49:42 +0530

    added JobNova logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import toast from "react-hot-toast";
+import logo from "../assets/logo.png";
 
 export default function Navbar() {
   const { user, logout } = useAuth();
